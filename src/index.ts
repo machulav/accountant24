1dcb8b9598a23fa300dcd71c3e04fdfbaee0f6fb
Volodymyr Machula <machulav@gmail.com>
2026-03-25 15:08:10 +0100

    add agentDir parameter to resource loader, settings, and session

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,14 @@ import { ACCOUNTANT24_HOME, accountant24Extension } from "./extension";
 async function main() {
   const resourceLoader = new DefaultResourceLoader({
     cwd: ACCOUNTANT24_HOME,
+    agentDir: ACCOUNTANT24_HOME,
     extensionFactories: [accountant24Extension],
     noSkills: true,
     noPromptTemplates: true,
   });
   await resourceLoader.reload();
 
-  const settingsManager = SettingsManager.create(ACCOUNTANT24_HOME);
+  const settingsManager = SettingsManager.create(ACCOUNTANT24_HOME, ACCOUNTANT24_HOME);
   settingsManager.applyOverrides({
     quietStartup: true,
     collapseChangelog: true,
@@ -27,6 +28,7 @@ async function main() {
 
   const { session, modelFallbackMessage } = await createAgentSession({
     cwd: ACCOUNTANT24_HOME,
+    agentDir: ACCOUNTANT24_HOME,
     resourceLoader,
     settingsManager,
     sessionManager: SessionManager.create(ACCOUNTANT24_HOME, join(ACCOUNTANT24_HOME, ".sessions")),
